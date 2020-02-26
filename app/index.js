3bd3d570f93dd4e5314145de3b4719160eab6baf
jobscale <jobscale@users.noreply.github.com>
2020-02-26 11:25:20 +0900

    get secret with partner

--- app/index.js
+++ app/index.js
@@ -1,21 +1,34 @@
 global.fetch = require('node-fetch');
-const env = require('./env');
 
 global.logger = console;
 const app = {
   url: 'https://dyn.value-domain.com/cgi-bin/dyn.fcg',
-  headers: {
+  fetchEnv() {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+    const pattern = [/=/g, '', 'base64'];
+    return fetch('https://partner/env.json', {
+      method: 'GET',
+      headers: { Cookie: 'X-AUTH=X0X0X0X0X0X0X0X' },
+    })
+    .then(res => res.json())
+    .then(res => JSON.parse(
+      Buffer.from(res.env.replace(...pattern), pattern[2]).toString(),
+    ).valueDomain);
   },
   fetchIP() {
     return fetch('http://inet-ip.info/ip')
     .then(res => res.text())
     .then(res => res.split('\n')[0]);
   },
-  async setAddress(ip) {
-    logger.log({ net: { ip } });
+  getData() {
+    return Promise.all([
+      this.fetchIP(),
+      this.fetchEnv(),
+    ]);
+  },
+  async setAddress(ip, env) {
     for (const host of env.hosts) {
       await fetch(`${app.url}?d=${env.domain}&p=${env.token}&h=${host}&i=${ip}`, {
-        headers: app.headers,
         method: 'GET',
       })
       .then(logger.info);
@@ -23,8 +36,8 @@ const app = {
     return 'OK';
   },
   main() {
-    return app.fetchIP()
-    .then(ip => app.setAddress(ip));
+    return app.getData()
+    .then(data => app.setAddress(...data));
   },
   start() {
     app.main()
