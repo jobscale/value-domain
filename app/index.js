d7a0a2db37fc8512e8bb314b7047f5e144581f7e
jobscale <jobscale@users.noreply.github.com>
2020-08-05 10:16:34 +0900

    with Class Object

--- app/index.js
+++ app/index.js
@@ -1,8 +1,12 @@
 global.fetch = require('node-fetch');
 
 global.logger = console;
-const app = {
-  url: 'https://dyn.value-domain.com/cgi-bin/dyn.fcg',
+
+class App {
+  constructor() {
+    this.url = 'https://dyn.value-domain.com/cgi-bin/dyn.fcg';
+  }
+
   fetchEnv() {
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
     const pattern = [/=/g, '', 'base64'];
@@ -14,34 +18,41 @@ const app = {
     .then(res => JSON.parse(
       Buffer.from(res.env.replace(...pattern), pattern[2]).toString(),
     ).valueDomain);
-  },
+  }
+
   fetchIP() {
     return fetch('http://inet-ip.info/ip')
     .then(res => res.text())
     .then(res => res.split('\n')[0]);
-  },
+  }
+
   getData() {
     return Promise.all([
       this.fetchIP(),
       this.fetchEnv(),
     ]);
-  },
+  }
+
   async setAddress(ip, env) {
     for (const host of env.hosts) {
-      await fetch(`${app.url}?d=${env.domain}&p=${env.token}&h=${host}&i=${ip}`, {
+      await fetch(`${this.url}?d=${env.domain}&p=${env.token}&h=${host}&i=${ip}`, {
         method: 'GET',
       })
+      .then(res => res.text())
       .then(logger.info);
     }
     return 'OK';
-  },
+  }
+
   main() {
-    return app.getData()
-    .then(data => app.setAddress(...data));
-  },
+    return this.getData()
+    .then(data => this.setAddress(...data));
+  }
+
   start() {
-    app.main()
-    .catch(logger.error);
-  },
-};
-app.start();
+    this.main()
+    .catch(e => logger.error({ e }));
+  }
+}
+
+new App().start();
